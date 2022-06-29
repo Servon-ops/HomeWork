2e5fea3127721c226309d06601c7201b816fc147
Ayaz Safiullin <ayaz-safiullin2012@yandex.ru>
2022-06-30 01:40:20 +0300

    [ADD]HomeWork1

---
+++ HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.homework1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.ConsoleMessage
+import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    var name: EditText? = null
+    var height: EditText? = null
+    var weight: EditText? = null
+    var age: EditText? = null
+    var button: Button? = null
+    var error: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.editTextName)
+        height = findViewById(R.id.editTextHeight)
+        weight = findViewById(R.id.editTextWeight)
+        age = findViewById(R.id.editTextAge)
+        button = findViewById(R.id.buttonCalculate)
+        error = findViewById(R.id.textError)
+
+        button?.setOnClickListener{checkValues()}
+    }
+    fun checkValues(){
+        error?.setVisibility(TextView.INVISIBLE)
+        var newHeight = height?.text.toString().toInt()
+        var newWeight = weight?.text.toString().toDouble()
+        var newAge = age?.text.toString().toInt()
+        if (newHeight > 0 && newHeight < 250 && newAge > 0 && newAge < 150 && newWeight > 0 && newWeight < 250) {
+            //Log.e("MainActivity", "Date For " + name?.text.toString() + " is " + newHeight * newAge * newWeight)}
+            error?.setVisibility(TextView.VISIBLE)
+            error?.setText("Date For " + name?.text.toString() + " is " + newHeight * newAge * newWeight)}
+        else {
+            //Log.e("MainActivity", "ERROOOOOR")
+            error?.setVisibility(TextView.VISIBLE)
+            error?.setText("the data is incorrect")
+        }
+    }
+}
